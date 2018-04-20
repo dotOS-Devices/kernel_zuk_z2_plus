a4c790266fd13632f0c0b11d6bc959e6e6c565bb
Connor O'Brien <connoro@google.com>
2018-04-20 14:03:03 -0700

    cpufreq: times: concurrent_active_time by pid
    In order to model the energy used by the cpu,
    we need to expose cpu time information to
    userspace. Output the time each pid spent running
    with 1 to num_possible_cpus online and not idle.
    
    We can use this information to blame pids and
    uids for the energy they are using.
    
    For instance, if 4 cores are online and active
    for 50ms and the pid is running on one of the
    cores, the pid should be blamed for 1/4 of the
    base energy used by the cpu when 1 or more cores is
    active. Using other proc files, the pid can
    also be blamed for the cost of keeping a cluster
    on and the cost of the individual cores it
    is using.
    
    This patch is based on commit
    a88a869505d33bb77ee7bca67602f5fe6048205d in the
    android-msm-wahoo-4.4 branch by Marissa Wall.
    
    It differs from the previous (reverted) commit
    b841887ffb80 on android-msm-bluecross-4.9 in that
    initialization & allocation of p->concurrent_active_time
    is split into separate functions.
    
    Bug: 68399339
    Bug: 74242328
    Test: cat /proc/<PID>/concurrent_active_time
    Change-Id: I3ee466278e0c8742777bed7f3fc5e58648920550
    Signed-off-by: Connor O'Brien <connoro@google.com>

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -3155,7 +3155,6 @@ static const struct pid_entry tgid_base_stuff[] = {
 #ifdef CONFIG_CPU_FREQ_TIMES
 	ONE("time_in_state", 0444, proc_time_in_state_show),
 	ONE("concurrent_active_time", 0444, proc_concurrent_active_time_show),
-	ONE("concurrent_policy_time", 0444, proc_concurrent_policy_time_show),
 #endif
 };
 
@@ -3544,7 +3543,6 @@ static const struct pid_entry tid_base_stuff[] = {
 #ifdef CONFIG_CPU_FREQ_TIMES
 	ONE("time_in_state", 0444, proc_time_in_state_show),
 	ONE("concurrent_active_time", 0444, proc_concurrent_active_time_show),
-	ONE("concurrent_policy_time", 0444, proc_concurrent_policy_time_show),
 #endif
 };
 
